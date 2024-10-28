224072c5370e7b03f77e165a9f39f4d3dbf9fe9b
Chelo1322 <ma_solis@live.concordia.ca>
2024-10-28 19:28:18 -0400

    Delete Profilemanagement.sql

--- Profilemanagement.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Users (
-    UserID INT PRIMARY KEY IDENTITY(1,1),
-    Username VARCHAR(50) NOT NULL UNIQUE,
-    Email VARCHAR(100) NOT NULL,
-    Role VARCHAR(20) CHECK (Role IN ('Manager', 'Waitress', 'Cook')),
-    PasswordHash VARCHAR(255) NOT NULL
-);
